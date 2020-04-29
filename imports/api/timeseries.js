4b432b325de1b0c451e4736c42630fe6356a8b37
dingheng4448 <ldh_1295@hotmail.com>
2020-04-30 01:30:43 +0800

    Process DB query with time range

--- imports/api/timeseries.js
+++ imports/api/timeseries.js
@@ -4,7 +4,10 @@ export const TimeseriesCollection = new Mongo.Collection('timeseries');
 
 // Publishes a subset of data to client based on function parameters
 if(Meteor.isServer){
-	Meteor.publish('queriedResults', function(startDate){
-		return TimeseriesCollection.find({ timestamp: startDate });
+	Meteor.publish('queriedResults', function(parameters){
+		var startTimestamp = parameters[0];
+		var endTimestamp = parameters[1];
+		
+		return TimeseriesCollection.find({ timestamp: { $gte : startTimestamp, $lte: endTimestamp } });
 	});
 }
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -12,7 +12,7 @@ class App extends React.Component {
 			<div>
 				<h3 id="header">sendh3lp's Room Temperature Monitoring Dashboard</h3>
 				<TimeSeriesGraphView />
-				{this.props.timeseries.map(item => <div>{item.timestamp_day}</div>)}
+				{this.props.timeseries.map(item => <div>{item.timestamp.toISOString()}</div>)}
 				<FloorplanView />
 			</div>
 		);
@@ -21,7 +21,10 @@ class App extends React.Component {
 
 // Subscribes to data from server based on user inputs
 export default withTracker(() => {
-	Meteor.subscribe('queriedResults', "2013-10-02");
+	var startTimestamp = new Date("2013-10-02T05:00:00");
+	var endTimestamp = new Date("2013-10-07T06:00:00");
+	
+	Meteor.subscribe('queriedResults', [startTimestamp, endTimestamp]);
 	return {
 		timeseries: TimeseriesCollection.find({}).fetch(),
 	};
